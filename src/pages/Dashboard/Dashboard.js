6973393881eaf587fc42726155d2cf4f2431d2b5
Diegocarrero <diegocar2413@gmail.com>
2023-08-21 19:08:21 -0400

    Add params variable and useeffect API call for current lesson

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,8 +1,21 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
+
+const API = process.env.REACT_APP_API_URL;
+// To be determined
 
 export default function Dashboard() {
-    const [currentLesson, setCurrentLesson] = useState({});
-    const [completedLessons, setCompletedLessons] = useState([]);
-    const [remainingLessons, setRemainingLessons] = useState([]);
+  const [currentLesson, setCurrentLesson] = useState({});
+  const [completedLessons, setCompletedLessons] = useState([]);
+  const [remainingLessons, setRemainingLessons] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios.get(`${API}/lessons/${id}`)
+      .then((res) => {
+        setCurrentLesson(res.data)
+      })
+      .catch((e) => console.warn('catch', e))
+  }, [])
 }
