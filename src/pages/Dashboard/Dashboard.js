0b4f150a79f705d77c75aa870ff2053837cc3aba
Diegocarrero <diegocar2413@gmail.com>
2023-08-21 19:46:18 -0400

    add completedLessons and lessonsInSameCategory functions

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -7,9 +7,10 @@ const API = process.env.REACT_APP_API_URL;
 
 export default function Dashboard() {
   const [currentLesson, setCurrentLesson] = useState({});
-//   const [completedLessons, setCompletedLessons] = useState([]);
+  const [completedLessons, setCompletedLessons] = useState([]);
 //   const [remainingLessons, setRemainingLessons] = useState([]);
   const [allLessons, setAllLessons] = useState([]);
+  const [nextLessons, setNextLessons] = useState([]);
 //   const { id } = useParams();
 
   useEffect(() => {
@@ -24,6 +25,21 @@ export default function Dashboard() {
   function currentLessonCard() {
     setCurrentLesson(
         allLessons.filter(lesson => lesson.completionStatus === 'in progress')
+    )
+  }
+
+  // Filters through all lessons to set completed lessons to the ones that have been completed
+  function completedLessons() {
+    setCompletedLessons(
+        allLessons.filter(lesson => lesson.completionStatus === 'completed')
+    )
+  }
+
+  // Filters through all lessons to set next lessons to the ones that share the same category as the current, and have not been started
+  function lessonsInSameCategory() {
+    setNextLessons(
+        allLessons.filter(lesson => lesson.category === currentLesson.category && 
+            lesson.completionStatus === 'not started')
     )
   }
 
