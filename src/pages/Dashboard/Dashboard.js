1f25671c2aeeceab1bde2cc4d6838ac03caa315e
Diegocarrero <diegocar2413@gmail.com>
2023-08-21 19:29:10 -0400

    add currentLesson function

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -7,15 +7,24 @@ const API = process.env.REACT_APP_API_URL;
 
 export default function Dashboard() {
   const [currentLesson, setCurrentLesson] = useState({});
-  const [completedLessons, setCompletedLessons] = useState([]);
-  const [remainingLessons, setRemainingLessons] = useState([]);
-  const { id } = useParams();
+//   const [completedLessons, setCompletedLessons] = useState([]);
+//   const [remainingLessons, setRemainingLessons] = useState([]);
+  const [allLessons, setAllLessons] = useState([]);
+//   const { id } = useParams();
 
   useEffect(() => {
-    axios.get(`${API}/lessons/${id}`)
+    axios.get(`${API}/lessons/`)
       .then((res) => {
-        setCurrentLesson(res.data)
+        setAllLessons(res.data)
       })
       .catch((e) => console.warn('catch', e))
   }, [])
+
+  // Filters through all lessons to set current lesson to the one in progress
+  function currentLessonCard() {
+    setCurrentLesson(
+        allLessons.filter(lesson => lesson.completionStatus === 'in progress')
+    )
+  }
+
 }
