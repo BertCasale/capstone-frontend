64b2571ebd5367085a2e3b6f649917cf4a41b980
Diegocarrero <diegocar2413@gmail.com>
2023-08-21 21:41:46 -0400

    Move currentCard functionality from dashboard to its own component

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import './Dashboard.css'
 
 const API = process.env.REACT_APP_API_URL;
 // To be determined
 
 export default function Dashboard() {
   const [user, setUser] = useState({})
-  const [currentLesson, setCurrentLesson] = useState({});
   const [completedLessons, setCompletedLessons] = useState([]);
 //   const [remainingLessons, setRemainingLessons] = useState([]);
   const [allLessons, setAllLessons] = useState([]);
@@ -15,19 +15,12 @@ export default function Dashboard() {
 //   const { id } = useParams();
 
   useEffect(() => {
-    axios.get(`${API}/lessons/`)
+    axios.get(`${API}/${user.username}/lessons/`)
       .then((res) => {
         setAllLessons(res.data)
       })
       .catch((e) => console.warn('catch', e))
   }, [])
-
-  // Filters through all lessons to set current lesson to the one in progress
-  function currentLessonCard() {
-    setCurrentLesson(
-        allLessons.filter(lesson => lesson.completionStatus === 'in progress')
-    )
-  }
 
   // Filters through all lessons to set completed lessons to the ones that have been completed
   function completedLessons() {
@@ -39,15 +32,18 @@ export default function Dashboard() {
   // Filters through all lessons to set next lessons to the ones that share the same category as the current, and have not been started
   function lessonsInSameCategory() {
     setNextLessons(
-        allLessons.filter(lesson => lesson.category === currentLesson.category && 
-            lesson.completionStatus === 'not started')
+      allLessons.filter(lesson => lesson.category === currentLesson.category && 
+        lesson.completionStatus === 'not started')
     )
   }
 
   return (
-    <section className="dashboard">
-        <h1 className="greeting">Welcome to your journey into art {user.username}!</h1>
-        <img className="profile-pic" src="profile_pic.jpg" alt="profile pic" width="200" height="200"/>
+    <section class="dashboard">
+      <section>
+        <h1 class="greeting">Welcome to your journey into art {user.username}!</h1>
+        <img class="profile-pic" src="profile_pic.jpg" alt="profile pic" width="200" height="200"/>
+      </section>
+      <CurrentLessonCard allLessons={allLessons}/>
     </section>
   )
 }
