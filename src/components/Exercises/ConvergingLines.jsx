53d8d85b8a1ffce591b9817881d53d74517ebf71
BertCasale <bthebest55@gmail.com>
2023-08-22 16:47:57 -0400

    ConvergingLines exercise interactive styling layout

--- src/components/Exercises/ConvergingLines.jsx
+++ src/components/Exercises/ConvergingLines.jsx
@@ -1,16 +1,19 @@
 import artwork from "../../assets/images/LePontdelEurope.jpg"
 import "./ConvergingLines.css";
 
-export default function ConvergingLines (){
+export default function ConvergingLines() {
 
-    return (<div>
+    return (<div className="converging-lines">
 
-        {/* the button that contains the image will give "incorrect_feedback" when clicked */}
-        <button className="incorrect-button"><img src={artwork} alt="Le Pont de l'Europe" className="artwork"/></button>
+        <div className="interactive-container">
+            {/* the button that contains the image will give "incorrect_feedback" when clicked */}
+            <img src={artwork} alt="Le Pont de l'Europe" className="artwork" />
 
-        {/* this button will be styled to be over the correct area on the image button */}
-        {/* will give the "correct_feedback" message when clicked, and allow the user to move on to the next section */}
-        <button className="correct-button"></button>
+            {/* this button will be styled to be over the correct area on the image button */}
+            {/* will give the "correct_feedback" message when clicked, and allow the user to move on to the next section */}
+            <button className="correct-button"></button>
+        </div>
+
 
         <h2 className="question">In the image above, click on the area that the converging lines seem to be directing your eyes toward.</h2>
 
