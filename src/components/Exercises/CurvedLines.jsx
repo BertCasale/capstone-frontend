7d50688715597eb3444941bec278e4ffffbcf3d2
BertCasale <bthebest55@gmail.com>
2023-08-27 17:08:13 -0400

    create CurvedLines files

---
+++ src/components/Exercises/CurvedLines.jsx
@@ -0,0 +1,9 @@
+
+import artwork from "../../assets/images/TheGreatWaveOffKanagawa.jpg"
+
+//setCompleted from Section
+export default function CurvedLines({ setCompleted }) {
+  return(<div>
+
+  </div>)
+}
