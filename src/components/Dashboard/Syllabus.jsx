a6fb89f545d54fa030a12eded0eaf49ffa6e1f95
Diegocarrero <diegocar2413@gmail.com>
2023-08-29 18:15:39 -0400

    add dummy data and container jsx for Syllabus

--- src/components/Dashboard/Syllabus.jsx
+++ src/components/Dashboard/Syllabus.jsx
@@ -3,21 +3,39 @@ import { useState } from "react"
 export default function Syllabus({ allLessons, client }) {
 
   const [selected, setSelected] = useState(false);
+  const modules = [
+    {
+      title: 'Elements of Art',
+      lessons: ['Lines Lesson', 'Shapes Lesson', 'Value Lesson']
+    },
+    {
+      title: 'Principles of Art',
+      lessons: ['Shades Lesson', 'Colors Lesson']
+    },
+    {
+      title: 'Advanced Fundamentals',
+      lessons: ['Paint Like Picasso Lesson', 'Paint Like Van Gogh Lesson']
+    }
+  ]
 
   function toggleSelected() {
     setSelected(!selected);
   }
-  
+
   return (
     <div className="column is-full">
-      <div className="panel">
-        <p className="panel-heading">
-          Collapsible Syllabus
-        </p>
-        <div className="panel-block">
 
+      <section className="container-accordion">
+        <div className="container">
+          <div className="is-multiline section-accordion">
+            {
+
+            }
+          </div>
         </div>
-      </div>
+      </section>
+
+
     </div>
   )
 }
