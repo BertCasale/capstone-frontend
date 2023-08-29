b8523bc83475d34ec8ebb3810073f41c290ac930
BertCasale <bthebest55@gmail.com>
2023-08-29 19:33:15 -0400

    LinesTest layout

--- src/components/Exercises/LinesTest.jsx
+++ src/components/Exercises/LinesTest.jsx
@@ -1,12 +1,23 @@
-import incorrectArtwork from "../../assets/images/RedBoatsAtArgenteuil.jpg"
+import incorrectArtwork from "../../assets/images/RedBoatsAtArgenteuil.jpg";
 import correctArtwork from "../../assets/images/BreezingUp.jpg";
 import "./LinesTest.css";
 
 //setCompleted, setAttempted, completed are all from sections
 export default function LinesTest({ setCompleted, setAttempted, completed }) {
-  return (<div>
-    <img src={correctArtwork} alt="Breezing Up" className="artwork"/>
-    <img src={incorrectArtwork} alt="Red Boats at Argenteuil" className="artwork"/>
+  
+  return (<div className="lines-test">
+
+    <div className="interactive-container">
+
+      <div className="image-div">
+
+        <img src={correctArtwork} alt="Breezing Up" className="artwork" style={completed ? {border:"green 3px solid"} : {}}/>
+        <img src={incorrectArtwork} alt="Red Boats at Argenteuil" className="artwork" style={completed ? {border:"red 3px solid"} : {}}/>
+
+      </div>
+
+    </div>
+
   </div>)
   
 
