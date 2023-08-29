23e527c972d345478855f017f30d07870af61799
BertCasale <bthebest55@gmail.com>
2023-08-29 18:47:47 -0400

    Zigzag exercise functionality

--- src/components/Exercises/ZigzagLines.jsx
+++ src/components/Exercises/ZigzagLines.jsx
@@ -1,9 +1,42 @@
-
+import { useState, useEffect } from "react";
 import artwork from "../../assets/images/JarWithZigzagPanels.jpg"
 import line from "../../assets/images/ZigzagOne.png"
 import "./ZigzagLines.css";
 
-export default function ZigzagLines() {
+//setCompleted from section
+export default function ZigzagLines({ setCompleted }) {
+  //state to make the lines nondraggable and set complete 
+  const [linesPlaced, setLinesPlaced] = useState({
+    line1: false,
+    line2: false,
+  });
+
+  //whenever a line is placed check to see if all are placed, to set complete
+  useEffect(() => {
+    if (linesPlaced.line1 && linesPlaced.line2) {
+      setCompleted(true);
+    }
+  }, [linesPlaced]);
+
+  //allow the div to accept a dropped element
+  function handleDragOver(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }
+
+  //what to do when starting to drag
+  function handleDragStart(event) {
+    event.dataTransfer.setData("image", event.target.id);
+    event.dataTransfer.effectAllowed = "move";
+  }
+
+  //what to do when the picture gets dropped
+  function handleDrop(event) {
+    const data = event.dataTransfer.getData("image");
+    event.target.appendChild(document.getElementById(data));
+    //change state to reflect which line was placed
+    setLinesPlaced({ ...linesPlaced, [`line${data[5]}`]: true });
+  }
 
   return (<div className="zigzag-lines">
 
@@ -15,10 +48,10 @@ export default function ZigzagLines() {
 
         {/* the box where the line can be drag and dropped into */}
         {/* once the line is dropped in, the next button will enable */}
-        <div className="empty-box first-box" id="line-1-box" >
+        <div className="empty-box first-box" onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleDrop(e)}>
 
         </div>
-        <div className="empty-box second-box" id="line-2-box" >
+        <div className="empty-box second-box" onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleDrop(e)}>
 
         </div>
 
@@ -28,13 +61,12 @@ export default function ZigzagLines() {
       <div className="starting-box">
         {/* disable the draggability if the line was placed */}
         {/* remove the rotation angle and the margins when placed */}
-        <img draggable={true} src={line} alt="zigzag line" id="line-1" className="line-1 line" />
+        <img draggable={!linesPlaced.line1} src={line} alt="zigzag line" id="line-1" className="line-1 line" onDragStart={(e) => handleDragStart(e)} style={linesPlaced.line1 ? {marginTop:"160px"} : {}}/>
 
-        <img draggable={true} src={line} alt="zigzag line" id="line-2" className="line-2" />
+        <img draggable={!linesPlaced.line2} src={line} alt="zigzag line" id="line-2" className="line-2 line" onDragStart={(e) => handleDragStart(e)} style={linesPlaced.line2 ? {marginTop:"160px"} : {}}/>
       </div>
 
     </div>
-
 
     <div className="content">
       <h3 className="question">Place the lines above on the areas with zigzag lines in the image above.</h3>
