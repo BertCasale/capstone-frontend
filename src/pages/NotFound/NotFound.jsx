63e435241c69777623ab54bb4ceb43886a99a38c
Diegocarrero <diegocar2413@gmail.com>
2023-08-30 21:17:03 -0400

    add button link back to home and contact button to 404 page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,4 +1,4 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 export default function NotFound() {
@@ -9,8 +9,8 @@ export default function NotFound() {
           <div className="column has-text-centered">
             <h1 className="title">404 Page Not Found</h1>
             <p className="subtitle">An unexpected error has occurred. Please contact the site owner.</p>
-            <a className="button">Home</a>
-            <a className="button">Contact</a>
+            <Link to={'/'} className="button is-rounded">Home</Link>
+            <Link to={''} className="button is-rounded">Contact</Link>
           </div>
         </div>
       </div>
