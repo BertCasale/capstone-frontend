b53cef3868fc2bd6e56dc9af94d645f50feaffd4
Diegocarrero <diegocar2413@gmail.com>
2023-08-30 21:10:38 -0400

    add NotFound route in App

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Landing from './pages/Landing/Landing'
 import Lesson from './pages/Lesson/Lesson'
 import Profile from './pages/Profile/Profile'
 import Sandbox from './pages/Sandbox/Sandbox'
+import NotFound from './pages/NotFound/NotFound'
 
 function App() {
 
@@ -20,6 +21,8 @@ function App() {
         {/* Is having two path params the best way around this? Could we use section_id alone? */}
         <Route path='/:username/profile' element={<Profile/>} />
         <Route path='/sandbox' element={<Sandbox/>} />
+
+        <Route path='*' element={<NotFound/>} />
       </Routes>
     </div>
   )
