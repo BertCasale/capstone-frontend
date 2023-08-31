b62eeaa644c1a3d51c1edf325774cf10db1bfde2
Diegocarrero <diegocar2413@gmail.com>
2023-08-30 21:10:38 -0400

    add NotFound route in App

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,6 @@ import Landing from './pages/Landing/Landing'
 import Lesson from './pages/Lesson/Lesson'
 import Profile from './pages/Profile/Profile'
 import Sandbox from './pages/Sandbox/Sandbox'
-import SignUpPage from './pages/SignUpPage'
-import Hamster from './pages/Hamster'
-import { db } from './services/config/firebase' // setup for firestore - may need to move this
-import { getDocs, collection} from 'firebase/firestore' // setup for firestore
-import { useEffect, useState } from 'react'
-
-
-
 
 function App() {
 
@@ -52,9 +44,6 @@ function App() {
         {/* Is having two path params the best way around this? Could we use section_id alone? */}
         <Route path='/:username/profile' element={<Profile/>} />
         <Route path='/sandbox' element={<Sandbox/>} />
-        <Route path='/signup' element={<SignUpPage/>} />
-        <Route path='/hamster' element={<Hamster/>} />
-
       </Routes>
     </div>
   )
