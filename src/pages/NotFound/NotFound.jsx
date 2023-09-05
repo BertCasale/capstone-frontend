f33f91511d86a53f0de2cefa88f6c69e9a01e13d
Diegocarrero <diegocar2413@gmail.com>
2023-09-05 17:38:47 -0400

    add sad squirrel image to NotFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import sadSquirrel from "../../assets/images/sad_squirrel.jpeg"
 
 export default function NotFound() {
   return (
@@ -8,6 +8,11 @@ export default function NotFound() {
         <div className="columns is-vcentered">
           <div className="column has-text-centered">
             <h1 className="title">404 Page Not Found</h1>
+            <div className="column is-flex is-justify-content-center">
+              <figure className="image is-128x128">
+                <img src={sadSquirrel} />
+              </figure>
+            </div>
             <p className="subtitle">An unexpected error has occurred. Please contact the site owner.</p>
             <Link to={'/'} className="button is-rounded">Home</Link>
             <Link to={''} className="button is-rounded">Contact</Link>
