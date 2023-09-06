28ff79166e648d42a174269ca79792e862c5c841
Diegocarrero <diegocar2413@gmail.com>
2023-09-05 21:20:50 -0400

    add useRef for canvasRef, useEffect for setting up the canvas visual layout

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useRef } from "react";
 
 
 export default function Canvas() {
+
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`
+
+    const context = canvas.getContext("2d")
+  }, [])
 
   const startDrawing = () => {
 
@@ -19,6 +32,7 @@ export default function Canvas() {
       onMouseDown={startDrawing}
       onMouseUp={endDrawing}
       onMouseMove={draw}
+      ref={canvasRef}
     >
       
     </canvas>
