0cf9934bfe26c853e0a576380c77d4df88058b23
Diegocarrero <diegocar2413@gmail.com>
2023-09-06 19:01:25 -0400

    pass height and width of canvas as props so that it can be rendered in different sizes, edit toolbar

---
+++ src/components/Canvas/Toolbar.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Toolbar() {
+  return (
+    <div>
+      <aside className="menu">
+        <ul className="menu-list">
+          <li><button>Hello</button></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </aside>
+    </div>
+  )
+}
--- src/pages/Sandbox/Sandbox.jsx
+++ src/pages/Sandbox/Sandbox.jsx
@@ -1,10 +1,12 @@
 import Canvas from "../../components/Canvas/Canvas"
+import Toolbar from "../../components/Canvas/Toolbar"
 
 export default function Sandbox() {
   return(
-    <>
-      <Canvas/>
-    </>
+    <section className="container">
+      <Toolbar/>
+      <Canvas canvasWidth={500} canvasHeight={500}/>
+    </section>
   )
 }
 
