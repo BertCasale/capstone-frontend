836a45c38c6ebfc8247136b0bab6f84fd7fe74fa
Diegocarrero <diegocar2413@gmail.com>
2023-09-05 21:13:04 -0400

    add canvas element and event listeners to Canvas

---
+++ src/components/Canvas/Canvas.jsx
@@ -0,0 +1,26 @@
+
+
+export default function Canvas() {
+
+  const startDrawing = () => {
+
+  }
+
+  const endDrawing = () => {
+
+  }
+
+  const draw = () => {
+
+  }
+
+  return (
+    <canvas
+      onMouseDown={startDrawing}
+      onMouseUp={endDrawing}
+      onMouseMove={draw}
+    >
+      
+    </canvas>
+  )
+}
