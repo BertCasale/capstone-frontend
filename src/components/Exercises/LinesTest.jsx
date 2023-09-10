19234629d850497b9c68fd46e0c0ef4df601eddc
BertCasale <bthebest55@gmail.com>
2023-09-10 17:53:15 -0400

    fix correct area not displaying after correct choice

--- src/components/Exercises/LinesTest.jsx
+++ src/components/Exercises/LinesTest.jsx
@@ -1,22 +1,24 @@
+import { useState } from "react";
 import incorrectArtwork from "../../assets/images/RedBoatsAtArgenteuil.jpg";
 import correctArtwork from "../../assets/images/BreezingUp.jpg";
 import "./LinesTest.css";
 
 //setCompleted, setAttempted, completed are all from sections
-export default function LinesTest({ setCompleted, setAttempted, completed }) {
-  
-    //what should be done when the correct area is clicked
-    function handleCorrectClick() {
-      setAttempted(true);
-      setCompleted(true);
-      event.target.style = "border: green 3px solid"
-    }
+export default function LinesTest({ setCompleted, setAttempted }) {
+  const [success, setSuccess] = useState(false);
 
-    //what should be done when the incorrect area is clicked
-    function handleIncorrectClick(event) {
-        setAttempted(true);
-        event.target.style = "border: red 3px solid"
-    }
+  //what should be done when the correct area is clicked
+  function handleCorrectClick() {
+    setAttempted(true);
+    setCompleted(true);
+    setSuccess(true);
+  }
+
+  //what should be done when the incorrect area is clicked
+  function handleIncorrectClick() {
+    setAttempted(true);
+    event.target.style = "border: red 3px solid"
+  }
 
   return (<div className="lines-test">
 
@@ -24,8 +26,8 @@ export default function LinesTest({ setCompleted, setAttempted, completed }) {
 
       <div className="artwork-div">
 
-        <img src={correctArtwork} alt="Breezing Up" className="artwork is-clickable" onClick={handleCorrectClick} />
-        <img src={incorrectArtwork} alt="Red Boats at Argenteuil" className="artwork is-clickable" onClick={handleIncorrectClick} />
+        <img src={correctArtwork} alt="Breezing Up" className={`artwork is-clickable ${success ? "completed-correct-selection" : ""} `} onClick={handleCorrectClick} />
+        <img src={incorrectArtwork} alt="Red Boats at Argenteuil" className={`artwork is-clickable ${success ? "completed-incorrect-selection" : ""} `} onClick={handleIncorrectClick} />
 
       </div>
 
