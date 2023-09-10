4975c8b1b1cef76152e32936a1009315cf84abbc
Diegocarrero <diegocar2413@gmail.com>
2023-09-10 09:17:13 -0400

    add lineWidth selection

--- src/components/Canvas/Toolbar.jsx
+++ src/components/Canvas/Toolbar.jsx
@@ -6,9 +6,11 @@ export default function Toolbar({ clearCanvas }) {
     <div>
       <aside className="menu">
         <ul className="menu-list">
+          <label for="stroke">Color Stroke</label>
+          <li><input name="stroke" type="color"></input></li>
+          <label for="lineWidth">Line Width</label>
+          <li><input name="lineWidth" type="number" value="5"></input></li>
           <li><button onClick={clearCanvas}>Clear</button></li>
-          <li><input id="stroke" name="stroke" type="color"></input></li>
-          <li></li>
           <li></li>
           <li></li>
         </ul>
