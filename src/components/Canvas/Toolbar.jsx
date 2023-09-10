bbcc9b851dbf87af160c8f9776508f1d800f03b0
Diegocarrero <diegocar2413@gmail.com>
2023-09-10 10:47:01 -0400

    add max and min values to line width

--- src/components/Canvas/Toolbar.jsx
+++ src/components/Canvas/Toolbar.jsx
@@ -9,7 +9,7 @@ export default function Toolbar({ clearCanvas, toggleColor, toggleLineWidth }) {
           <label htmlFor="stroke">Color Stroke</label>
           <li><input name="stroke" type="color" onChange={toggleColor}></input></li>
           <label htmlFor="lineWidth">Line Width</label>
-          <li><input name="lineWidth" type="number" defaultValue="5" onChange={toggleLineWidth}></input></li>
+          <li><input name="lineWidth" type="number" defaultValue="5" onChange={toggleLineWidth} max="10" min="1"></input></li>
           <li><button onClick={clearCanvas}>Clear</button></li>
           <li></li>
           <li></li>
