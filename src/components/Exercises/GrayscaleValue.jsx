ba2b149bb6dd6d98b1fec885e5cdbcecdac05aea
BertCasale <bthebest55@gmail.com>
2023-09-10 12:54:35 -0400

    create grayscale exercise files

---
+++ src/components/Exercises/GrayscaleValue.jsx
@@ -0,0 +1,22 @@
+import artwork from "../../assets/images/GrayscaleShades.jpg"
+import "./GrayscaleValue.css";
+
+//setCompleted, completed, and setAttempted come from section
+export default function ConvergingLines({ setCompleted }) {
+
+
+
+  return (<div className="converging-lines">
+
+    <div className="interactive is-flex is-justify-content-center">
+
+      <div className="artwork-div">
+
+        
+
+      </div>
+      
+    </div>
+
+  </div>)
+}
