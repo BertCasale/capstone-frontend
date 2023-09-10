ebd85855d05fe789d96b3a2875bb6dc2929e120f
BertCasale <bthebest55@gmail.com>
2023-09-10 13:20:23 -0400

    grayscaleValues exexercise functionality

--- src/components/Exercises/GrayscaleValue.jsx
+++ src/components/Exercises/GrayscaleValue.jsx
@@ -1,10 +1,14 @@
 import artwork from "../../assets/images/GrayscaleShades.jpg"
 import "./GrayscaleValue.css";
+import {JigsawPuzzle} from "react-jigsaw-puzzle/lib";
+import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
 
 //setCompleted, completed, and setAttempted come from section
 export default function ConvergingLines({ setCompleted }) {
 
-
+  function handlePuzzleSolved() {
+    setCompleted(true);
+  }
 
   return (<div className="converging-lines">
 
@@ -12,7 +16,13 @@ export default function ConvergingLines({ setCompleted }) {
 
       <div className="artwork-div">
 
-        
+        <JigsawPuzzle
+          imageSrc={artwork}
+          rows={1}
+          columns={9}
+          onSolved={handlePuzzleSolved}
+          className="artwork"
+        />
 
       </div>
       
