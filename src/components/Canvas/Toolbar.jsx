9ffbaaef8c00e9b38f90c95c1561b7573df8cb06
Diegocarrero <diegocar2413@gmail.com>
2023-09-10 11:25:17 -0400

    add erase mode

--- src/components/Canvas/Toolbar.jsx
+++ src/components/Canvas/Toolbar.jsx
@@ -1,6 +1,6 @@
 
 
-export default function Toolbar({ clearCanvas, toggleColor, toggleLineWidth }) {
+export default function Toolbar({ clearCanvas, toggleColor, toggleLineWidth, eraseMode }) {
 
   return (
     <div>
@@ -10,9 +10,8 @@ export default function Toolbar({ clearCanvas, toggleColor, toggleLineWidth }) {
           <li><input name="stroke" type="color" onChange={toggleColor}></input></li>
           <label htmlFor="lineWidth">Line Width</label>
           <li><input name="lineWidth" type="number" defaultValue="5" onChange={toggleLineWidth} max="10" min="1"></input></li>
+          <li><button onClick={eraseMode}>Erase</button></li>
           <li><button onClick={clearCanvas}>Clear</button></li>
-          <li></li>
-          <li></li>
         </ul>
       </aside>
     </div>
