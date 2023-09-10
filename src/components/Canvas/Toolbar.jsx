d585a9f5ffad87db068b379081381ec36a3fad3d
Diegocarrero <diegocar2413@gmail.com>
2023-09-10 09:35:05 -0400

    add toggleColor to Canvas, pass down to Toolbar and add to stroke input

--- src/components/Canvas/Toolbar.jsx
+++ src/components/Canvas/Toolbar.jsx
@@ -1,13 +1,13 @@
 
 
-export default function Toolbar({ clearCanvas }) {
+export default function Toolbar({ clearCanvas, toggleColor }) {
 
   return (
     <div>
       <aside className="menu">
         <ul className="menu-list">
           <label for="stroke">Color Stroke</label>
-          <li><input name="stroke" type="color"></input></li>
+          <li><input name="stroke" type="color" onChange={toggleColor}></input></li>
           <label for="lineWidth">Line Width</label>
           <li><input name="lineWidth" type="number" value="5"></input></li>
           <li><button onClick={clearCanvas}>Clear</button></li>
