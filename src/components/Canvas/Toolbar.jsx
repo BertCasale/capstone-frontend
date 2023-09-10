f18d68109adb78ae1706436d3d48470b28d01260
Diegocarrero <diegocar2413@gmail.com>
2023-09-10 09:58:59 -0400

    edit canvas and toolbar, add defaultValue to lineWidth

--- src/components/Canvas/Toolbar.jsx
+++ src/components/Canvas/Toolbar.jsx
@@ -1,15 +1,15 @@
 
 
-export default function Toolbar({ clearCanvas, toggleColor, toggleLineWidth }) {
+export default function Toolbar({ clearCanvas, toggleColor, toggleLineWidth, contextRef }) {
 
   return (
     <div>
       <aside className="menu">
         <ul className="menu-list">
-          <label for="stroke">Color Stroke</label>
+          <label htmlFor="stroke">Color Stroke</label>
           <li><input name="stroke" type="color" onChange={toggleColor}></input></li>
-          <label for="lineWidth">Line Width</label>
-          <li><input name="lineWidth" type="number" value="5" onChange={toggleLineWidth}></input></li>
+          <label htmlFor="lineWidth">Line Width</label>
+          <li><input name="lineWidth" type="number" defaultValue="5" onChange={toggleLineWidth}></input></li>
           <li><button onClick={clearCanvas}>Clear</button></li>
           <li></li>
           <li></li>
