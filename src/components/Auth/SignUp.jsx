200ded8e4c9ed07998467acf20c4f8e3804ed327
Keri Armstrong <115671612+keriarmstrong@users.noreply.github.com>
2023-09-12 21:02:17 -0400

    added additional fields to signup form: email, reg date, role radio option

--- src/components/Auth/SignUp.jsx
+++ src/components/Auth/SignUp.jsx
@@ -3,80 +3,86 @@ import { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
 import { auth } from "../../services/config/firebase"
 import { createUserWithEmailAndPassword } from "firebase/auth"
-import { addDoc, collection } from 'firebase/firestore'
-import { db } from '../../services/config/firebase'
+// import { addDoc, collection } from 'firebase/firestore'
+// import { db } from '../../services/config/firebase'
 import axios from 'axios'
 const API = import.meta.env.VITE_API_URL;
 
 // import { useAuth } from '../../contexts/AuthContexts'
 
-console.log(API)
 export default function SignUp() {
-
-  
-  
-  const userCollectionRef = collection(db, "users");
+ 
+  // const userCollectionRef = collection(db, "users");
   const navigate = useNavigate()
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  //UseState variable
+  // const [username, setUsername] = useState('');
+  // const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [radioSelection, setRadioSelection] = useState('student')
 
   const [client, setClient] = useState({
-    registration_datetime: '2023-09-12T01:59:14.000Z',
+    registration_datetime: '',
     username: '',
-    email: 'email@example.com',
+    email: '',
     password: '',
     profile_picture: 'none',
-    role: 'student',
+    role: '',
   });
 
 
-
-  const newClient = async () => { // Make it async
+//POST request to the backend for creating new user account
+  const newClient = async (newUserDetails) => { // Make it async
     try {
-      await axios.post(`${API}/clients`, client); // Use client object directly
-      // navigate('/dashboard');
+      await axios.post(`${API}/clients`, newUserDetails); // Use client object directly
+      navigate('/dashboard');
     } catch (error) {
       console.log(error);
     }
   }
-  // const [errorMessage, setErrorMessage] = useState('');
 
 
   //---------------------------------------
+  //handler function for signup
   const handleSignUp = async (e) => {
     e.preventDefault();
    
     try {
       // Treat the username as the email and create a user account in firebase Auth
       // await createUserWithEmailAndPassword(auth, `${username}@domain.com`, password)
-      await createUserWithEmailAndPassword(auth, `${client.username}@domain.com`, client.password)
+      
+      //uses firebase creatUserWithEmailAndPassword builtin function
+      await createUserWithEmailAndPassword(auth, `${client.email}`, client.password)
       // console.log(`Signup successful`);
-      .then((userCredential) => {
-        const user = userCredential.user;
+      
+      //response data(userDetails) is used to filter for specifics like UID, Date, Email, Password
+      .then((userDetails) => {
+        const user = userDetails.user;
         const uid = user.uid;
+        //date start - date conversion
         const registrationDate = user.metadata.creationTime
         const dateObj = new Date(registrationDate)
         const registered = dateObj.toISOString();
+        //date end
         const userEmail = user.email
-        // const recordDate = {...client, registration_datetime: registered}
-      
+        //update the registration_datetime key in client object
+        const recordDate = {...client, registration_datetime: registered}
+        newClient(recordDate)
+
         console.log(`new user created with UID: ${uid} Username: ${userEmail} registed on ${registered}`);
- 
-        console.log(client)
-        
+        console.log(recordDate);
+        console.log(client)   
       });
 
-      await newClient();
-      //add the new user data to firestore db
-      await addDoc(userCollectionRef, { username: username, password: password });
-      //clear fields
-      setUsername('');
-      setPassword('')
-      setConfirmPassword('')
-      //redirect user
-      navigate('/')
+      // // await newClient(recordDate);
+      // //add the new user data to firestore db
+      // await addDoc(userCollectionRef, { username: username, password: password });
+      // //clear fields
+      // setUsername('');
+      // setPassword('')
+      // setConfirmPassword('')
+      // //redirect user
+      // navigate('/')
 
     } catch (error) {
       console.error('Signup error', error.message);
@@ -89,8 +95,8 @@ export default function SignUp() {
 
 //-----------------------
 
-  const handleTextChange = (e) => {
-    setClient({...client, [e.target.id]: e.target.value })
+  const handleTextChange = async (e) => {
+    await setClient({...client, [e.target.id]: e.target.value })
   }
 
   return (
@@ -103,6 +109,17 @@ export default function SignUp() {
             value={client.username}
             color="link"
             placeholder='Username'
+            // onChange={(e) => setUsername(e.target.value)}
+            onChange={handleTextChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Form.Input
+            id='email'
+            type='text'
+            value={client.email}
+            color="link"
+            placeholder='Email'
             // onChange={(e) => setUsername(e.target.value)}
             onChange={handleTextChange}
           />
@@ -127,6 +144,25 @@ export default function SignUp() {
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </Form.Field>
+        <Form.Control>
+          <Form.Radio
+        
+            value="Student"
+            color="link"
+            checked={radioSelection === "Student"}
+            onChange={((e) =>{setRadioSelection(e.target.value); setClient({...client, role: "Student"});})}>
+              Student
+            </Form.Radio>
+          <Form.Radio
+          
+            value="Administrator"
+            color="link"
+            checked={radioSelection === "Administrator"}
+            onChange={((e) =>{setRadioSelection(e.target.value); setClient({...client, role: "Administrator"});})}>
+              Administrator
+            </Form.Radio>
+       
+        </Form.Control>
         <Form.Field>
           <Button className='is-link'>Sign Up</Button>
         </Form.Field>
