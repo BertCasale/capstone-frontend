e17f32e4fd924c9e45532d4c41d9a9efdec05ec8
Diegocarrero <diegocar2413@gmail.com>
2023-09-13 19:21:10 -0400

    when a mode is toggled, it shuts all others off

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -559,6 +559,7 @@ export default function Canvas({ canvasWidth, canvasHeight }) {
     // if eraseMode is being turned off, sets the brush color to the previous one
     if (lineMode) setLineMode(false);
     if (rectangleMode) setRectangleMode(false);
+    if (circleMode) setCircleMode(false);
     setEraseMode(!eraseMode);
   }
 
@@ -581,6 +582,7 @@ export default function Canvas({ canvasWidth, canvasHeight }) {
       // if eraseMode was on, sets it to off and sets the brush color to the previous one
     }
     if (rectangleMode) setRectangleMode(false);
+    if (circleMode) setCircleMode(false);
     setLineMode(!lineMode);
   }
 
@@ -598,6 +600,7 @@ export default function Canvas({ canvasWidth, canvasHeight }) {
       // if eraseMode was on, sets it to off and sets the brush color to the previous one
     }
     if (lineMode) setLineMode(false);
+    if (circleMode) setCircleMode(false);
     setRectangleMode(!rectangleMode);
   }
 
@@ -611,6 +614,8 @@ export default function Canvas({ canvasWidth, canvasHeight }) {
       contextRef.current.strokeStyle = brushColor;
       // if eraseMode was on, sets it to off and sets the brush color to the previous one
     }
+    if (lineMode) setLineMode(false);
+    if (rectangleMode) setRectangleMode(false);
     setCircleMode(!circleMode);
   }
 
