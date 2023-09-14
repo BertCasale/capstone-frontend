23aba493cb49a3aae642903882eed49bc009897c
BertCasale <bthebest55@gmail.com>
2023-09-13 20:53:47 -0400

    ValueTest added functionality

--- src/components/Exercises/ValueTest.jsx
+++ src/components/Exercises/ValueTest.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
 import artwork from "../../assets/images/ValueTest.jpg";
 import "./ValueTest.css";
 
 //setCompleted from Section
 export default function ValueTest({ setCompleted, setAttempted }) {
+  //keep track of the colors in each area, along with their correct color
+  const [areasFilled, setAreasFilled] = useState([
+    {correctColor: "#BFBFBF", correct: false},
+    {correctColor: "#3F3F3F", correct: false},
+    {correctColor: "#7F7F7F", correct: false},
+    {correctColor: "#000000", correct: false},
+    {correctColor: "#FFFFFF", correct: false},
+  ]);
+  //keep track of how many areas are filled
+  const [areaFilledAmount, setAreaFilledAmount] = useState(0);
+
+  //when either the areas filled amount or the areas filled change, 
+  //check if the areas are all correct and set either attempted or completed
+  useEffect(() => {
+    if (areaFilledAmount === 5) {
+      let solved = areasFilled.every((area) => {
+        return area.correct === true;
+      })
+
+      if (solved){
+        setCompleted(true);
+      } else {
+        setAttempted(true);
+      }
+    }
+    
+  }, [areaFilledAmount, areasFilled])
 
   //allow the divs on the image to recieve a dropped element
   function handleDragOver(event) {
@@ -13,19 +41,43 @@ export default function ValueTest({ setCompleted, setAttempted }) {
   //when starting to drag, allo
   function handleDragStart(event) {
     event.dataTransfer.setData("shade", event.target.id);
-    event.dataTransfer.effectAllowed = "copy";
+    event.dataTransfer.effectallowed = "copy";
   }
 
   //set the background color of the area on the image to be the same color as the div dropped onto it
   function handleDrop(event) {
-    let blankArea = event.target;
+    let blankArea = event.target.style
+    //area index value
+    let areaNumber = event.target.id[5] - 1;
+    //get the correct color for the specific area from state
+    let correctColorValue = areasFilled[areaNumber].correctColor;
+
+    //if the are was empty, then increase area filled amount by 1
+    if (blankArea.backgroundColor === "") {
+      setAreaFilledAmount(areaFilledAmount + 1);
+    }
     const data = event.dataTransfer.getData("shade");
-    blankArea.style.backgroundColor = `#${data}`;
+    blankArea.backgroundColor = data;
+
+    let allAreas = [...areasFilled];
+    //if the color is correct, set the correct boolean in the areaFilled state for that area to true, otherwise set to false
+    if (correctColorValue === data) {
+      allAreas[areaNumber].correct = true;
+      setAreasFilled([...allAreas]);
+    } else {
+      allAreas[areaNumber].correct = false;
+      setAreasFilled([...allAreas]);
+    }
   }
 
   //remove the color from the div on the image
+  //lower the amount of the areas filled amount state by one
   function handleClick(event) {
-    event.target.style.backgroundColor = "";
+    let areaColor = event.target.style;
+    if (areaColor.backgroundColor !== "") {
+      areaColor.backgroundColor = "";
+      setAreaFilledAmount(areaFilledAmount - 1);
+    }
   }
 
   return (<div className="value-test">
@@ -38,6 +90,7 @@ export default function ValueTest({ setCompleted, setAttempted }) {
 
         <div
           className="empty-area first-area is-flex is-align-items-center is-clickable"
+          id="area-1"
           onDragOver={(e) => handleDragOver(e)}
           onDrop={(e) => handleDrop(e)}
           onClick={(e) => handleClick(e)}>
@@ -45,6 +98,7 @@ export default function ValueTest({ setCompleted, setAttempted }) {
 
         <div
           className="empty-area second-area is-flex is-align-items-center is-clickable"
+          id="area-2"
           onDragOver={(e) => handleDragOver(e)}
           onDrop={(e) => handleDrop(e)}
           onClick={(e) => handleClick(e)}>
@@ -52,6 +106,7 @@ export default function ValueTest({ setCompleted, setAttempted }) {
 
         <div
           className="empty-area third-area is-flex is-align-items-center is-clickable"
+          id="area-3"
           onDragOver={(e) => handleDragOver(e)}
           onDrop={(e) => handleDrop(e)}
           onClick={(e) => handleClick(e)}>
@@ -59,6 +114,7 @@ export default function ValueTest({ setCompleted, setAttempted }) {
 
         <div
           className="empty-area fourth-area is-flex is-align-items-center is-clickable"
+          id="area-4"
           onDragOver={(e) => handleDragOver(e)}
           onDrop={(e) => handleDrop(e)}
           onClick={(e) => handleClick(e)}>
@@ -66,6 +122,7 @@ export default function ValueTest({ setCompleted, setAttempted }) {
 
         <div
           className="empty-area fifth-area is-flex is-align-items-center is-clickable"
+          id="area-5"
           onDragOver={(e) => handleDragOver(e)}
           onDrop={(e) => handleDrop(e)}
           onClick={(e) => handleClick(e)}>
@@ -76,16 +133,15 @@ export default function ValueTest({ setCompleted, setAttempted }) {
       {/* the box the contains the shades at the start */}
       <div className="starting-box is-flex is-justify-content-center">
 
-        <div className="shade white" title="White" id="FFFFFF" draggable onDragStart={(e) => handleDragStart(e)}></div>
+        <div className="shade white" title="White" id="#FFFFFF" draggable onDragStart={(e) => handleDragStart(e)}></div>
 
-        <div className="shade light-gray" title="Light Gray" id="BFBFBF" draggable onDragStart={(e) => handleDragStart(e)}></div>
+        <div className="shade light-gray" title="Light Gray" id="#BFBFBF" draggable onDragStart={(e) => handleDragStart(e)}></div>
 
-        <div className="shade middle-gray" title="Middle Gray" id="7F7F7F" draggable onDragStart={(e) => handleDragStart(e)}></div>
+        <div className="shade middle-gray" title="Middle Gray" id="#7F7F7F" draggable onDragStart={(e) => handleDragStart(e)}></div>
 
-        <div className="shade dark-gray" title="Dark Gray" id="3F3F3F" draggable onDragStart={(e) => handleDragStart(e)}></div>
+        <div className="shade dark-gray" title="Dark Gray" id="#3F3F3F" draggable onDragStart={(e) => handleDragStart(e)}></div>
 
-        <div className="shade black" title="Black" id="000000" draggable onDragStart={(e) => handleDragStart(e)}></div>
-
+        <div className="shade black" title="Black" id="#000000" draggable onDragStart={(e) => handleDragStart(e)}></div>
 
       </div>
 
