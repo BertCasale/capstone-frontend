825deba71c6235211aa833314fd153f16d56d47d
Diegocarrero <diegocar2413@gmail.com>
2023-09-17 10:07:36 -0400

    Move all code for image generator to its own separate component

---
+++ src/components/Canvas/ImageGenerator.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+export default function ImageGenerator() {
+
+  const [textPrompt, setTextPrompt] = useState('');
+  const [generatedImage, setGeneratedImage] = useState(null);
+
+  /* IMAGE GENERATOR FUNCTION */
+
+  const generateImageFromText = async () => {
+    // Sends the text prompt to the AI image generation service
+    try {
+      const response = await fetch('AI_IMAGE_GENERATOR_API_URL', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ textPrompt }),
+      })
+      if (response.ok) {
+        const generatedImageData = await response.json();
+
+        // Displays the generated image
+        setGeneratedImage(generatedImageData.url);
+      } else {
+        console.error('Error generating image:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter a text prompt"
+        value={textPrompt}
+        onChange={(e) => setTextPrompt(e.target.value)}
+      />
+      <button onClick={generateImageFromText}>Generate Image</button>
+    </div>
+  )
+}
