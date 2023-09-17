6732d36c0e9a893775cc5425602f8aaa50a71ea0
Diegocarrero <diegocar2413@gmail.com>
2023-09-17 17:32:10 -0400

    add loading message for ImageGenerator

--- src/components/Canvas/ImageGenerator.jsx
+++ src/components/Canvas/ImageGenerator.jsx
@@ -5,12 +5,13 @@ import { openai } from '../../services/config/dalleAPI'
 export default function ImageGenerator() {
 
   const [textPrompt, setTextPrompt] = useState(''); // Text prompt to be sent to image generator API
-  const [generatedImage, setGeneratedImage] = useState(''); // Url of AI generated image sent back from API based on textPrompt 
+  const [generatedImage, setGeneratedImage] = useState(''); 
+  // Url of AI generated image sent back from API based on textPrompt 
+  const [loading, setLoading] = useState(false);
 
-  /* IMAGE GENERATOR FUNCTION */
-
-  const generateImageFromText = async () => {
-    // Requests image url from Dall-E based on text prompt
+  // Requests image url from Dall-E based on text prompt and sets it to be displayed
+  const generateImage = async () => {
+    setLoading(true); // Displays loading message
     try {
       const response = await openai.images.generate({
         prompt: promptHelper(textPrompt),
@@ -19,10 +20,8 @@ export default function ImageGenerator() {
       })
 
       if (response.created) {
-        const generatedImageUrl = await response.data[0].url;
-
-        // Displays the generated image
-        setGeneratedImage(generatedImageUrl);
+        setLoading(false); // Hides loading message
+        setGeneratedImage(response.data[0].url); // Displays the generated image
       } else {
         console.error('Error generating image:', response.statusText);
       }
@@ -37,21 +36,32 @@ export default function ImageGenerator() {
   }
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter a text prompt for inspiration"
-        value={textPrompt}
-        onChange={(e) => setTextPrompt(e.target.value)}
-      />
-      <button onClick={generateImageFromText}>Generate Image</button>
+    <div className='field'>
+
+      <label className='label is-large'>Generate an Image using Dall-E</label>
+
+      <div className="control">
+        <input
+          className='input is-large'
+          type="text"
+          placeholder="Enter a prompt for inspiration"
+          value={textPrompt}
+          onChange={(e) => setTextPrompt(e.target.value)}
+        />
+      </div>
+
+      <button className='button' onClick={generateImage}>Generate Image</button>
+
+      {loading && (
+        <h2>Generating Image... Please wait...</h2>
+      )}
 
       {generatedImage && (
-        <div>
-          <h2>Generated Image</h2>
+        <div className='mt-4'>
           <img src={generatedImage} alt='Generated Image' />
         </div>
       )}
+
     </div>
   )
 }
