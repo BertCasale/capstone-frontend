3d4aca1dc166fae3f0af1543f3e8170a02bf17a3
Diegocarrero <diegocar2413@gmail.com>
2023-09-18 20:58:12 -0400

    revert commenting out everything related to auth

--- src/components/NavBar/UserAuthBtn.jsx
+++ src/components/NavBar/UserAuthBtn.jsx
@@ -1,49 +1,49 @@
-// // import { auth } from "../../services/config/firebase"
-// import SignOut from "../Auth/SignOut";
+// import { auth } from "../../services/config/firebase"
+import SignOut from "../Auth/SignOut";
 
-// // eslint-disable-next-line react/prop-types
-// export default function UserAuthBtn({ setIsModalActive, authUser, user, setUser}) {
-//   // setIsModalActive prop being passed from NavBar component
+// eslint-disable-next-line react/prop-types
+export default function UserAuthBtn({ setIsModalActive, authUser, user, setUser}) {
+  // setIsModalActive prop being passed from NavBar component
 
-// // const user = authUser
+// const user = authUser
 
-//  const handleButtonState = () => {
+ const handleButtonState = () => {
  
-//   // if (isLoading) {
-//   //   return <p>Loading...</p>;
-//   // } else
-//    if (authUser) {
-//     return (
-//       <div>
-//         {/* <p style={{ color: 'white' }}>{user}</p> */}
-//         <p style={{ color: 'white' }}>logged in</p>
-//         {/* <button onClick={handleLogout}>Logout</button> */}
-//         {/* Render the user profile icon here */}
-//         {/* <img
-//           src={user.photoURL || 'default-avatar.jpg'}
-//           alt="User Profile"
-//           width="50"
-//         /> */}
-//         <SignOut user={user} setUser={setUser} authUser={authUser}/>
-//       </div>
-//     );
-//   } else {
-//     return (
-//       <button
-//         className={`button is-link-outlined is-rounded`}
-//         onClick={() => {
-//           setIsModalActive(true);
-//         }}
-//       >
-//         <span>Sign in</span>
-//       </button>
-//     );
-//   }
-// }
+  // if (isLoading) {
+  //   return <p>Loading...</p>;
+  // } else
+   if (authUser) {
+    return (
+      <div>
+        {/* <p style={{ color: 'white' }}>{user}</p> */}
+        <p style={{ color: 'white' }}>logged in</p>
+        {/* <button onClick={handleLogout}>Logout</button> */}
+        {/* Render the user profile icon here */}
+        {/* <img
+          src={user.photoURL || 'default-avatar.jpg'}
+          alt="User Profile"
+          width="50"
+        /> */}
+        <SignOut user={user} setUser={setUser} authUser={authUser}/>
+      </div>
+    );
+  } else {
+    return (
+      <button
+        className={`button is-link-outlined is-rounded`}
+        onClick={() => {
+          setIsModalActive(true);
+        }}
+      >
+        <span>Sign in</span>
+      </button>
+    );
+  }
+}
 
-//   return (
-//     <div>
-//       {handleButtonState()}  
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      {handleButtonState()}  
+    </div>
+  )
+}
