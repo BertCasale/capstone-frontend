d22ded08739bd6c39fa14d0a3ff3a627fc043188
Diegocarrero <diegocar2413@gmail.com>
2023-09-19 17:05:04 -0400

    Revert "comment out everything with auth"
    This reverts commit 6aaa8348b57ddbfc2e39fe10bb8d401db15d487e.

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,16 +1,16 @@
 // import SignUp from "../components/NavBar/SignUp"
-// import { Container } from 'react-bulma-components'
-// import SignUp from "../components/Auth/SignUp"
+import { Container } from 'react-bulma-components'
+import SignUp from "../components/Auth/SignUp"
 
-// export default function SignUpPage() {
-//   return (
-//     <div>
+export default function SignUpPage() {
+  return (
+    <div>
       
-//         <h1>Sign Up</h1>
+        <h1>Sign Up</h1>
  
-//       <Container className="p-3">
-//         <SignUp />
-//       </Container>
-//     </div>
-//   )
-// }
+      <Container className="p-3">
+        <SignUp />
+      </Container>
+    </div>
+  )
+}
