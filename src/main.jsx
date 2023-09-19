a7b231b62bc76f249d2e4c84012fae53cd40a38f
Diegocarrero <diegocar2413@gmail.com>
2023-09-18 20:58:12 -0400

    revert commenting out everything related to auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-// import { AuthProvider} from './contexts/AuthContext.jsx'
+import { AuthProvider} from './contexts/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
-      {/* <AuthProvider> */}
+      <AuthProvider>
       <App />
-      {/* </AuthProvider> */}
+      </AuthProvider>
     </React.StrictMode>
   </BrowserRouter>
 )
