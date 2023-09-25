5a8b287149d87ae407b004302dd34119ffe19686
Diegocarrero <diegocar2413@gmail.com>
2023-09-25 19:51:37 -0400

    edit LessonInPath

--- src/components/Dashboard/LessonInPath.jsx
+++ src/components/Dashboard/LessonInPath.jsx
@@ -1,24 +1,47 @@
-import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { BsStar, BsStarFill } from 'react-icons/bs'
 
 export default function LessonInPath({ lesson }) {
-  return (
-    <div className="level"> 
 
-      <div className="level-left">
+  const [isPressed, setIsPressed] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsPressed(true);
+    setTimeout(() => {
+      setIsPressed(false);
+    }, 100); // Resets the pressed state after a short delay 
+  }
+
+  return (
+    <div className="level pb-5"> 
+      
+        <button 
+          className="pushable"
+          onClick={handleButtonClick}
+        >
+          <span className="shadow"></span>
+          <span className="edge"></span>
+          <span className="front"><BsStar height={300}/></span>
+        </button>
+      <Link to={`/lesson/${lesson.id}`} >
+      </Link>
+      
+      {/* <span className="has-text-weight-bold">Start Lesson</span> */}
+      
+
+      {/* <div className="level-left">
+        
         <figure className="image level-item is-24x24">
           <img src="https://bulma.io/images/placeholders/96x96.png" alt="Module Icon"/>
         </figure>        
         <h6 className="level-item">{lesson.title}</h6>
+        
       </div>
       
       <div className="level-right">
-        <Link to={`/lesson/${lesson.id}`} className="level-item">
-          <button className="button is-rounded is-success">
-            <span className="has-text-weight-bold">Start Lesson</span>
-          </button>
-        </Link>
-      </div>
+        
+      </div> */}
 
     </div>
   )
