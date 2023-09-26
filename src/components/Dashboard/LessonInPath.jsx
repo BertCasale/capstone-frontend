6a1afe40fb88329c63f929bf27fc8dc909cb1dcb
Diegocarrero <diegocar2413@gmail.com>
2023-09-25 21:42:11 -0400

    add basic pop-up to LessonInPath buttons

--- src/components/Dashboard/LessonInPath.jsx
+++ src/components/Dashboard/LessonInPath.jsx
@@ -5,18 +5,41 @@ import './LessonInPath.css'
 
 export default function LessonInPath({ lesson }) {
 
+  const [selected, setSelected] = useState(false);
+
+  function toggleSelected() {
+    setSelected(!selected);
+  }
+
   return (
     <div className="level pb-5"> 
       
         <button 
           className="pushable"
+          onClick={toggleSelected}
         >
           <span className="shadow"></span>
           <span className="edge"></span>
-          <span className="front"><BsStar height={300}/></span>
+          <span className="front">
+            {selected ? <BsStarFill/> : <BsStar height={300}/>}
+          </span>
         </button>
-      <Link to={`/lesson/${lesson.id}`} >
-      </Link>
+
+        {selected ? 
+          <div className='popup'>
+            <span className={`popuptext ${selected ? 'show': ''}`}>
+              <Link to={`/lesson/${lesson.id}`} >
+                <button className='button is-rounded is-link'>
+                  <p className='has-text-weight-bold'>Start Lesson</p>
+                </button>
+              </Link>
+            </span>
+          </div> 
+        : null}
+
+        
+          
+        
 
     </div>
   )
