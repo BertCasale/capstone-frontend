abe5622d4df98542112e8631178ed9af1328fc4b
BertCasale <bthebest55@gmail.com>
2023-09-27 16:05:44 -0400

    VerticalLines line fixes and mobile functionality

--- src/components/Exercises/VerticalLines.jsx
+++ src/components/Exercises/VerticalLines.jsx
@@ -18,34 +18,114 @@ export default function VerticalLines({ setCompleted }) {
     }
   }, [linesPlaced]);
 
-  //allow the div to accept a dropped element
-  function handleDragOver(event) {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = "move";
-  }
+  //event for clicking on the line
+  function handleMouseDown(event) {
+    //the line that we are moving
+    let movingLine = event.target;
+    //create a variable for the dropping area
+    let elementBelow = null;
 
-  //what to do when starting to drag
-  function handleDragStart(event) {
-    event.dataTransfer.setData("image", event.target.id);
-    event.dataTransfer.effectAllowed = "move";
-  }
+    //get the spot where the mouse is clicked relative to the line
+    let shiftX = event.clientX - movingLine.getBoundingClientRect().left;
+    let shiftY = event.clientY - movingLine.getBoundingClientRect().top;
 
-  //what to do when the picture gets dropped
-  function handleDrop(event) {
-    //get the id of the box and the image
-    let emptyBoxId = event.target.id;
-    const data = event.dataTransfer.getData("image");
-    //check if the line dropped belongs to the corresponding box
-    if (emptyBoxId === `${data}-box`) {
-      event.target.appendChild(document.getElementById(data));
-      //change state to reflect which line was placed
-      setLinesPlaced({ ...linesPlaced, [`line${data[5]}`]: true });
+    //prepare to move
+    movingLine.style.position = 'absolute';
+    movingLine.style.zIndex = 1000;
+
+    //move the line, using the mouse as the reference point
+    function moveAt(pageX, pageY) {
+      movingLine.style.left = pageX - shiftX + 'px';
+      movingLine.style.top = pageY - shiftY + 'px';
     }
+    
+    function handleMove(event) {
+      //move the line everytime the mouse moves
+      moveAt(event.pageX, event.pageY);
+
+      //get the element directly under the mouse
+      movingLine.hidden = true;
+      elementBelow = document.elementFromPoint(event.clientX, event.clientY);
+      movingLine.hidden = false;
+
+    }
+
+    //move the line when first clicking
+    moveAt(event.pageX, event.pageY);
+
+    //add the mousemove event listener to the document to move the line
+    document.addEventListener("mousemove", handleMove);
+
+    //when releasing the mouse
+    movingLine.onmouseup = function() {
+      //check if the elemnt directly below is the correct empty box div
+      if (elementBelow.id === `${movingLine.id}-box`){
+        //append the line to the div 
+        elementBelow.appendChild(movingLine);
+
+        setLinesPlaced({ ...linesPlaced, [movingLine.id]: true });
+      }
+      //return the line to starting position
+      movingLine.style.left = "";
+      movingLine.style.top = "";
+      movingLine.style.position = "";
+      movingLine.style.zIndex = "";
+
+      //remove the event listener from the document
+      document.removeEventListener("mousemove", handleMove);
+      movingLine.onmouseup = null;
+    };
+
+  }
+
+  //same as above, but for touch screens
+  function handleTouch(event) {
+    let movingLine = event.target;
+    let elementBelow = null;
+
+    let shiftX = event.touches[0].clientX - movingLine.getBoundingClientRect().left;
+    let shiftY = event.touches[0].clientY - movingLine.getBoundingClientRect().top;
+
+    movingLine.style.position = 'absolute';
+    movingLine.style.zIndex = 1000;
+
+    function moveAt(pageX, pageY) {
+      movingLine.style.left = pageX - shiftX + 'px';
+      movingLine.style.top = pageY - shiftY + 'px';
+    }
+
+    moveAt(event.touches[0].pageX, event.touches[0].pageY);
+
+    function handleMove(event) {
+
+      moveAt(event.touches[0].pageX, event.touches[0].pageY);
+
+      movingLine.hidden = true;
+      elementBelow = document.elementFromPoint(event.touches[0].clientX, event.touches[0].clientY);
+      movingLine.hidden = false;
+    }
+
+    document.addEventListener("touchmove", handleMove);
+
+    movingLine.ontouchend = function() {
+      if (elementBelow.id === `${movingLine.id}-box`){
+        elementBelow.appendChild(movingLine);
+        setLinesPlaced({ ...linesPlaced, [movingLine.id]: true });
+      }
+
+      movingLine.style.left = "";
+      movingLine.style.top = "";
+      movingLine.style.position = "";
+      movingLine.style.zIndex = "";
+
+      document.removeEventListener("touchmove", handleMove);
+      movingLine.ontouchend = null;
+    };
   }
 
   return (<div className="vertical-lines">
 
-    <div className="interactive is-flex is-align-items-center is-flex-direction-column">
+    <div className="interactive is-flex is-justify-content-center">
 
       <div className="artwork-div">
 
@@ -53,17 +133,28 @@ export default function VerticalLines({ setCompleted }) {
 
         {/* the box where the line can be drag and dropped into */}
         {/* once the line is dropped in, the next button will enable */}
-        <div className="empty-box first-box" id="line-1-box" onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleDrop(e)}>
-
+        <div 
+          className="empty-box first-box" 
+          id="line1-box" 
+        >
         </div>
-        <div className="empty-box second-box" id="line-2-box" onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleDrop(e)}>
 
+        <div 
+          className="empty-box second-box" 
+          id="line2-box" 
+        >
         </div>
-        <div className="empty-box third-box" id="line-2-box" onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleDrop(e)}>
 
+        <div 
+          className="empty-box third-box" 
+          id="line2-box" 
+        >
         </div>
-        <div className="empty-box fourth-box" id="line-1-box" onDragOver={(e) => handleDragOver(e)} onDrop={(e) => handleDrop(e)}>
 
+        <div 
+          className="empty-box fourth-box" 
+          id="line1-box" 
+        >
         </div>
 
       </div>
@@ -71,8 +162,25 @@ export default function VerticalLines({ setCompleted }) {
       {/* the box the contains the line at the start */}
       <div className="starting-box is-flex is-justify-content-center">
         {/* disable the draggability if the line was placed */}
-        <img draggable={!linesPlaced.line1} src={line} alt="vertical line" id="line-1" className="line-1 line is-clickable" onDragStart={(e) => handleDragStart(e)} />
-        <img draggable={!linesPlaced.line2} src={line} alt="vertical line" id="line-2" className="line-2 line is-clickable" onDragStart={(e) => handleDragStart(e)} />
+        <img 
+          draggable="false" 
+          src={line} 
+          alt="vertical line" 
+          id="line1" 
+          className="line-1 line is-clickable"  
+          onMouseDown={linesPlaced.line1 ? null : (e) => handleMouseDown(e)}
+          onTouchStart={linesPlaced.line1 ? null : (e) => handleTouch(e)}
+        />
+
+        <img 
+          draggable="false"
+          src={line} 
+          alt="vertical line" 
+          id="line2" 
+          className="line-2 line is-clickable" 
+          onMouseDown={linesPlaced.line2 ? null : (e) => handleMouseDown(e)}
+          onTouchStart={linesPlaced.line2 ? null : (e) => handleTouch(e)}
+        />
       </div>
 
     </div>
