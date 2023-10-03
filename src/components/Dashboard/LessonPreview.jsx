ac97ddd83c6fe4eb86c928ad9f899a2620f583f2
Diegocarrero <diegocar2413@gmail.com>
2023-10-03 19:34:52 -0400

    comment out lesson preview component

--- src/components/Dashboard/LessonPreview.jsx
+++ src/components/Dashboard/LessonPreview.jsx
@@ -1,9 +1,13 @@
-
+import preview from "../../assets/images/Preview_Lines.png"
 
 export default function LessonPreview({ nextLesson }) {
   return (
-    <div className="box">
-      {/* {nextLesson.preview} */}
+    <div className="columns is-centered pt-1">
+      <div className="column is-full">
+        <div className="box">
+          <img className="pop-image" src={preview} alt="lesson preview image" />
+        </div>
+      </div>
     </div>
   )
 }
